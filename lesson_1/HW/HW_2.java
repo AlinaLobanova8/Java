dc8c95baad808b397d3a64c1f35afe1d069e68bd
Alina <alobanova8888@gmail.com>
2023-03-10 01:20:00 +0300

    Homework 3

--- lesson_1/HW/HW_2.java
+++ lesson_1/HW/HW_2.java
@@ -3,6 +3,10 @@ package Java.lesson_1.HW;
 import java.util.Arrays;
 
 public class HW_2 {
+    /*
+     * Задать целочисленный массив, состоящий из элементов 0 и 1.
+     * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
     public static void main(String[] args) {
         int[] ints = new int[] {1, 0, 1, 0, 0, 1, 1, 0, 1};
         for (int i = 0; i < ints.length; i++) {
